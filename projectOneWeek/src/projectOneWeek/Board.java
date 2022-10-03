fc2def83509fbe5e16af7a4cef3cf3b9756fbf17
Jawneee <jawnabellera25@gmail.com>
2022-10-03 10:29:22 -0500

    Made the base GUI

--- projectOneWeek/src/projectOneWeek/Board.java
+++ projectOneWeek/src/projectOneWeek/Board.java
@@ -1,5 +1,40 @@
 package projectOneWeek;
 
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 public class Board {
+	
+	boolean wall = false;
+	
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	JButton[] grid;
+	
+	public Board() {
+		
+		
+		frame = new JFrame("Mouse Maze");
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setSize(800,800);
+		frame.setLocationRelativeTo(null);
+		
+		
+		panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		grid = new JButton[100];
+		String[] ac = new String[100];
+		
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+		
+		
+	}
 
 }
--- projectOneWeek/src/projectOneWeek/MainBoard.java
+++ projectOneWeek/src/projectOneWeek/MainBoard.java
@@ -3,6 +3,8 @@ package projectOneWeek;
 public class MainBoard {
 
 	public static void main(String[] args) {
+		
+		Board b = new Board();
 		System.out.println("Hello World");
 
 		System.out.println("hi");
