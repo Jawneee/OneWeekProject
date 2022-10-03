8b5e54122907f2132c8ac52c38deda46cedac07b
Jordan <jordaanyan@gmail.com>
2022-10-03 10:38:56 -0500

    rest

--- projectOneWeek/src/projectOneWeek/Mouse.java
+++ projectOneWeek/src/projectOneWeek/Mouse.java
@@ -1,24 +1,8 @@
 package projectOneWeek;
 
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-
 
 public class Mouse {
 	
 	
-	
-	
-	
-	public Mouse() {
-		frame = new JFrame("Mouse Maze ");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-	}
+
 }
