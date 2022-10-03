7b7a28e1ee7b1ba6510241059f9a76018a6fd48a
Jawneee <jawnabellera25@gmail.com>
2022-10-03 10:16:47 -0500

    A test

---
+++ projectOneWeek/src/projectOneWeek/MainBoard.java
@@ -0,0 +1,10 @@
+package projectOneWeek;
+
+public class MainBoard {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ projectOneWeek/src/projectOneWeek/Mouse.java
@@ -0,0 +1,5 @@
+package projectOneWeek;
+
+public class Mouse {
+
+}
