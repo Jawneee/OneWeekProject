1e8ecc4f5c0d9880f14a56d0830a67f867fb778b
abramsm <abramsm@wcmp15vs015lt.blads.ca>
2022-10-03 10:22:12 -0500

    hi

--- projectOneWeek/src/projectOneWeek/MainBoard.java
+++ projectOneWeek/src/projectOneWeek/MainBoard.java
@@ -4,12 +4,12 @@ public class MainBoard {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
+
 		System.out.println("hi");
-=======
+
 
 		System.out.println("This is Jordan");
->>>>>>> branch 'main' of https://github.com/Jawneee/OneWeekProject.git
+
 	}
 
 }
