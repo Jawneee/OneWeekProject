a571c8aa8d18994d01bdb1005e10865c2fa1b195
abramsm <abramsm@wcmp15vs015lt.blads.ca>
2022-10-03 10:47:31 -0500

    sca

--- projectOneWeek/src/projectOneWeek/Mouse.java
+++ projectOneWeek/src/projectOneWeek/Mouse.java
@@ -2,7 +2,23 @@ package projectOneWeek;
 import java.util.Random;
 
 public class Mouse {
-	Random r = new Random();
+	private int x,y;
+	private int [][] mouse = new int [10][10];
 	
+	Mouse(int xNum, int yNum) {
+		x=xNum;
+		y=yNum;
+		for(int i =0; i<mouse.length(); i++) {
+			for(int j = 0; j<mouse[0].length();j++) {
+				mouse[i][j]=0;
+			}
+		}
 	
+		
+		
+		
+		
+		
+		
+	}
 }
