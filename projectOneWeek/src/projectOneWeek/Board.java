049576ecb3fa5761c42f8d8ac17e94f563f6bed2
Chris <loewenchris@outlook.com>
2022-10-03 11:00:56 -0500

    I made "walls" work.

--- projectOneWeek/src/projectOneWeek/Board.java
+++ projectOneWeek/src/projectOneWeek/Board.java
@@ -1,11 +1,10 @@
 package projectOneWeek;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 
 public class Board {
 	
@@ -46,7 +45,30 @@ public class Board {
             grid[i].setName("tile");
             panel.add(grid[i], c);
             System.out.println("Made Grid");
+            
+            
+            grid[i].addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent e) {
+					int temp;
+					
+					temp=Integer.valueOf(e.getActionCommand());
+					
+					if(grid[temp].getName() == "tile") {
+						grid[temp].setBackground(Color.BLACK);
+					}
+					
+				}
+            	
+            	
+			});
+            
 		}
+		
+		
+		
+		
+		
 		
 		
 		frame.setContentPane(panel);
