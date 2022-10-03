7f8b34e13f0f71adf0e1f60541639f0b3712cc18
Chris <loewenchris@outlook.com>
2022-10-03 10:43:47 -0500

    got rid of printlns in main

--- projectOneWeek/src/projectOneWeek/MainBoard.java
+++ projectOneWeek/src/projectOneWeek/MainBoard.java
@@ -5,12 +5,7 @@ public class MainBoard {
 	public static void main(String[] args) {
 		
 		Board b = new Board();
-		System.out.println("Hello World");
-
-		System.out.println("hi");
-
-
-		System.out.println("This is Jordan");
+		
 
 	}
 
