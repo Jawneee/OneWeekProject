bde32c468e84718174605eb9891f9bbd296da524
Jordan <jordaanyan@gmail.com>
2022-10-04 10:24:28 -0500

    Deletes stuff

--- projectOneWeek/src/projectOneWeek/Mouse.java
+++ projectOneWeek/src/projectOneWeek/Mouse.java
@@ -1,23 +1,13 @@
 package projectOneWeek;
 
-import java.util.Random;
-
 public class Mouse {
 
 	private int x = 1;
-	private int[] mouse = new int[10];
+
 
 	Mouse(int xNum) {
 
 		xNum = x;
-
-		for (int i = 0; i < mouse.length; i++) {
-			mouse[i]=0;
-			
-			
-			
-		}
-
 
 	}
 	
