10a5d11ea0b9ed6e22518b47372cd68b1b3d7638
abramsm <abramsm@wcmp15vs015lt.blads.ca>
2022-10-04 10:22:14 -0500

    i made method

--- projectOneWeek/src/projectOneWeek/Mouse.java
+++ projectOneWeek/src/projectOneWeek/Mouse.java
@@ -21,7 +21,24 @@ public class Mouse {
 
 	}
 	
-
-
+	public int up() {
+		x=-19;
+		return x;
+	}
+	
+	public int down() {
+		x=19;
+		return x;
+	}
+	
+	public int right() {
+		x=1;
+		return x;
+	}
+	public int left() {
+		x=-1;
+		return x;
+	}
+	
 }
 
