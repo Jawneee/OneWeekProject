2ee2dc61e870e715035b32ae781a76e013af6562
Jawneee <jawnabellera25@gmail.com>
2022-10-19 10:52:07 -0500

    Everything is working now

--- projectOneWeek/src/projectOneWeek/Board.java
+++ projectOneWeek/src/projectOneWeek/Board.java
@@ -1496,7 +1496,7 @@ public class Board {
 				}
 
 
-				
+				//asd
 				if(grid[mousePos-20].getBackground() == Color.gray && grid[mousePos-1].getBackground() == Color.gray && grid[mousePos+1].getBackground() == Color.gray && grid[mousePos+20].getBackground() == Color.black) {
 					grayClear();
 				}else if(grid[mousePos-20].getBackground() == Color.gray && grid[mousePos+1].getBackground() == Color.gray && grid[mousePos+20].getBackground() == Color.gray && grid[mousePos-1].getBackground() == Color.BLACK) {
@@ -1512,6 +1512,10 @@ public class Board {
 				}else if(grid[mousePos+1].getBackground() == Color.black && grid[mousePos+20].getBackground() == Color.BLACK && grid[mousePos-20].getBackground() == Color.black && grid[mousePos-1].getBackground() == Color.black) {
 					grayClear();
 				}
+				
+				if(grid[mousePos-1].getBackground() == Color.gray && grid[mousePos-20].getBackground() == Color.gray && grid[mousePos+20].getBackground() == Color.black && grid[mousePos+1].getBackground() == Color.black) {
+					grayClear();
+				}else if(grid[mousePos+1].getBackground() == Color.gray && grid[mousePos-20].getBackground() == Color.gray && grid[mousePos+20].getBackground() == Color.black && grid[mousePos-1].getBackground() == Color.black)
 				
 				if(grid[mousePos+20].getBackground() == Color.gray && grid[mousePos-20].getBackground() == Color.gray && grid[mousePos+1].getBackground() == Color.black && grid[mousePos-1].getBackground() == Color.black) {
 					grayClear();
